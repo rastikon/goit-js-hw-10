8d33200c1d91351d3793792ea770455c48b96a11
Rostyslav Kononenko <rastikon@gmail.com>
2022-09-15 17:20:40 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name/ukraine')
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+  });
