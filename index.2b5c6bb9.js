28056d440bcfe592d23bdf6fde82a1d23869f88f
rastikon <40177045+rastikon@users.noreply.github.com>
2022-09-15 18:53:08 +0000

    Deploying to gh-pages from @ rastikon/goit-js-hw-10@1b977fe7950fef73af8f905feed8fc484d2e7c32 🚀

--- index.2b5c6bb9.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/ukraine").then((function(n){return n.json()})).then((function(n){console.log(n)}));
-//# sourceMappingURL=index.2b5c6bb9.js.map
--- index.eeb5ec2a.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/ukraine").then((e=>e.json())).then((e=>{console.log(e)}));
-//# sourceMappingURL=index.eeb5ec2a.js.map
